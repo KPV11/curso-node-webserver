4add546a1bb11eefb2d357beea2a4881c8229811
Kevin Palomino <kpalominov11@gmail.com>
2022-01-06 17:07:36 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const hbs = require('hbs');
+
+const app = express()
+const port = 8080;
+
+
+
+
+//Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+// Servir contenido estatico
+app.use(express.static('public'));
+
+app.get('/',(req, res) => {
+  res.render('home',{
+      nombre: 'Kevin Palomino',
+      titulo: 'Curso Node'  
+  });
+});
+
+app.get('/generic',(req, res) => {
+  res.render('generic',{
+      nombre: 'Kevin Palomino',
+      titulo: 'Curso Node'  
+  });
+});
+
+app.get('/elements',(req, res) => {
+  res.render('elements',{
+      nombre: 'Kevin Palomino',
+      titulo: 'Curso Node'  
+  });
+});
+
+app.get('*',(req, res) => {
+  res.sendFile(__dirname + '/public/404.html')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
